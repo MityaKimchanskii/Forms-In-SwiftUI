d693b5d1bb413e6831918543f56b4fa15360cc74
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-17 15:51:56 -0700

    Create Forms in SwiftUI

--- Forms-In-SwiftUI/ContentView.swift
+++ Forms-In-SwiftUI/ContentView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var scheduled: Bool = false
+    @State private var manuallyEnabledTomorrow: Bool = false
+    @State private var colorTemperature: CGFloat = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    Toggle(isOn: $scheduled) {
+                        Text("Scheduled")
+                    }
+                    
+                    HStack {
+                        
+                        VStack {
+                            Text("From")
+                            Text("To")
+                        }
+                        
+                        Spacer()
+                        
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            VStack {
+                                Text("Sanset").foregroundColor(.blue)
+                                Text("Sunrise").foregroundColor(.blue)
+                            }
+                        }.fixedSize()
+
+                        
+                        
+                    }
+                    
+                } header: {
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ")
+                }
+                Section {
+                    Toggle(isOn: $manuallyEnabledTomorrow) {
+                        Text("Manually Enable Until Tomorrow")
+                    }
+                } header: {
+                    Text("")
+                }
+                
+                Section {
+                    
+                    HStack {
+                        
+                        Text("Less Warm")
+                        Slider(value: $colorTemperature)
+                        Text("More Warm")
+                    }
+                    
+                    
+                } header: {
+                    Text("Color Temperature")
+                }
+
+
+
+            }
+        }
+           
     }
 }
 
