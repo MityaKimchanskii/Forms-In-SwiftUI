940be3feb743755a3944a30682fafe33a5b17b95
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-17 15:15:36 -0700

    Initial Commit

---
+++ Forms-In-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Forms-In-SwiftUI
+//
+//  Created by Mitya Kim on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Forms-In-SwiftUI/Forms_In_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forms_In_SwiftUIApp.swift
+//  Forms-In-SwiftUI
+//
+//  Created by Mitya Kim on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Forms_In_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
